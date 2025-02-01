a1c9cafa2867b7cb2d25825df23a45f15330d46d
ritethatguy <147250401+ritepro@users.noreply.github.com>
2025-02-01 16:52:40 +1000

    Create secretgame.py

---
+++ secretgame.py
@@ -0,0 +1,78 @@
+import time
+import os
+
+haha = 12, 23, 12, 43, 12
+
+print(
+"""
++================================+
+| This is just a normal code + - |
+| ready to play the games    + - |
+| Those who know             + - |
+| good luck lol              + - |
+| HINT (secret)              + - |
++================================+
+""")
+print("press enter to start")
+input()
+os.system("cls")
+print("starting up in 4 seconds i hope you looked at the hint")
+time.sleep(4)
+os.system("cls")
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+os.system("cls")
+print("Please wait while we calculate Client")
+time.sleep(1)
+
+largest_number = max(number1, number2)
+os.system("cls")
+
+print(largest_number)
+time.sleep(2)
+os.system("cls")
+
+if largest_number == largest_number:
+    time.sleep(2)
+    os.system("cls")
+    print("congrats Client")
+    time.sleep(3)
+    
+print("now that you have waited congrats")
+time.sleep(2)
+os.system("cls")
+congrats = input("Enter the discord account to hack: ")
+
+if congrats == congrats:
+    print("please stick with us while we analize the info")
+    time.sleep(2)
+    os.system("cls")
+    mango = input("Is this the correct discord account to hack? yes or no: ")
+    if mango == "yes":
+        while True:
+            print("get a job Bro")
+    
+if mango == "secret":
+    print("Well you know the secret wait a second")
+    time.sleep(3)
+    os.system("cls")
+    print("STARTING UP SECRET CODE")
+    time.sleep(2)
+    secret = input("Want to see what is behind all the secrets yes or no: ")
+    if secret == "yes":
+        os.system("cls")
+        print("one moment while we load the secret documents.")
+        time.sleep(3)
+        os.system("cls")
+        print("the Secret is sigma boy is coming for you!")
+
+    if secret == "no":
+        os.system("cls")
+        print("THERE IS ONLY ONE OPTION")
+        time.sleep(3)
+        os.system("cls")
+        while True:
+            print(haha, haha, haha)
+    
+
